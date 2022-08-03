42ebd70701ed865a14f25d7d7025c2eca5e22473
yjy44 <jy.yun@bitnine.net>
2022-08-03 17:48:29 +0900

    Version Check Module is added

--- .github/workflows/FindVer.py
+++ .github/workflows/FindVer.py
@@ -9,12 +9,13 @@ list = os.popen('ls -d */ -l | awk \'{print $9}\'').read()
 arr = list.strip().split('/\n')
 
 ret = arr[0]
-
 for x in arr:
-    
+    ##prevstr = x.replace('../..', '')
+
     comp = int(x.replace('v', '').replace('.','').replace('/',''))
     if(int(comp) > int(prev)):
         prev = comp
         ret = x
 
 print('/' + ret)
+
